537f12a985eb0b05ecfe794e4cf80d531f07e543
Julian AMG <julian.amg@hotmail.com>
2025-10-03 14:27:04 -0500

    fixing clic lib version to support python 3.9

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from .models import Todo, TodoCreate, TodoItem, TodoUpdate
-from .database import get_db
+from models import Todo, TodoCreate, TodoItem, TodoUpdate
+from database import get_db
 
 router = APIRouter()
 
